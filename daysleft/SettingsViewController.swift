4f241140c9c0d7d758bcef3f5898d5e17b758ef8
John Pollard <johnp@bravelocation.com>
2015-02-17 17:04:55 +0000

    Initial basic UI code

---
+++ daysleft/SettingsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  SettingsViewController.swift
+//  daysleft
+//
+//  Created by John Pollard on 17/02/2015.
+//  Copyright (c) 2015 Brave Location Software. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textTitle: UITextField!
+    @IBOutlet weak var dateEnd: UIDatePicker!
+    @IBOutlet weak var dateStart: UIDatePicker!
+    @IBOutlet weak var switchWeekdaysOnly: UISwitch!
+    
+    var model: DaysLeftModel = DaysLeftModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.textTitle.text = model.title
+        self.dateEnd.date = model.end
+        self.dateStart.date = model.start
+        self.switchWeekdaysOnly.on = model.weekdaysOnly
+        
+        // Don't let the start date be later than the end date
+        self.dateStart.maximumDate = self.dateEnd.date
+        self.dateEnd.minimumDate = self.dateStart.date
+        
+        // Set up the delegate of text field for handling return below
+        self.textTitle.delegate = self
+    }
+    
+    @IBAction func textTitleChanged(sender: AnyObject) {
+        model.title = self.textTitle.text
+    }
+    
+    @IBAction func dateEndChanged(sender: AnyObject) {
+        self.validateAndSaveDates()
+    }
+    
+    @IBAction func dateStartChanged(sender: AnyObject) {
+        self.validateAndSaveDates()
+    }
+
+    @IBAction func switchWeekdaysOnlyChanged(sender: AnyObject) {
+        model.weekdaysOnly = self.switchWeekdaysOnly.on
+    }
+    
+    // Hides the keyboard if touch anywhere outside text box
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    
+    // Hides the keyboard if return is pressed
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true);
+        return false;
+    }
+    
+    func validateAndSaveDates() {
+        // Update the model
+        model.start = self.dateStart.date
+        model.end = self.dateEnd.date
+            
+        // Update the date restrictions too
+        self.dateStart.maximumDate = self.dateEnd.date
+        self.dateEnd.minimumDate = self.dateStart.date
+    }
+}
+
--- daysleft/ViewController.swift
+++ daysleft/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelDaysLeft: UILabel!
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    let model: DaysLeftModel = DaysLeftModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Check if this is the first run
+        if (self.model.firstRun < self.model.currentFirstRun)
+        {
+            // If it is initialise the model dat
+            self.model.start = NSDate()
+            self.model.end = NSCalendar.currentCalendar().dateByAddingUnit(NSCalendarUnit.DayCalendarUnit, value: 11, toDate: self.model.start, options: nil)!
+            self.model.title = ""
+            self.model.weekdaysOnly = false
+            
+            // Save the first run once working
+            self.model.firstRun = self.model.currentFirstRun
+        }
+
+        self.updateViewFromModel()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func returnFromSettings(segue: UIStoryboardSegue) {
+        self.updateViewFromModel()
+    }
+    
+    func updateViewFromModel() {
+        let now: NSDate = NSDate()
+        self.labelDaysLeft.text = String(format: "%d", self.model.DaysLeft(now))
+        
+        let titleSuffix: String = (count(self.model.title) == 0 ? "the end" : self.model.title)
+        let titleDays: String = self.model.weekdaysOnly ? "weekdays" : "days"
+        self.labelTitle.text = String(format: "%@ until %@", titleDays, titleSuffix)
+    }
 
 }
 
