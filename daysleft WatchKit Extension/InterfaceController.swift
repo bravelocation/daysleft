218fa2a278d3033bfdff8e42ca074e3e02cf3cd7
John Pollard <johnp@bravelocation.com>
2015-03-22 12:25:48 +0000

    Initial WatchKit code and settings text on screen

---
+++ daysleft WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,52 @@
+//
+//  InterfaceController.swift
+//  daysleft WatchKit Extension
+//
+//  Created by John Pollard on 22/03/2015.
+//  Copyright (c) 2015 Brave Location Software. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import daysleftlibrary
+
+class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var labelTitle: WKInterfaceLabel!
+    @IBOutlet weak var labelPercentDone: WKInterfaceLabel!
+    @IBOutlet weak var imageProgress: WKInterfaceImage!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        
+        self.updateViewData()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    
+    private func updateViewData() {
+        let now: NSDate = NSDate()
+        let model: DaysLeftModel = DaysLeftModel()
+        let daysLeft: Int = model.DaysLeft(now)
+        let titleSuffix: String = (countElements(model.title) == 0 ? "left" : "until " + model.title)
+        let titleDays: String = model.weekdaysOnly ? "weekdays" : "days"
+        
+        self.labelTitle.setText(String(format: "%d %@ %@", daysLeft, titleDays, titleSuffix))
+        
+        let percentageDone: Float = (Float(model.DaysGone(now)) * 100.0) / Float(model.DaysLength)
+        self.labelPercentDone.setText(String(format:"%3.0f%% done", percentageDone))
+        
+        // TODO: Set the progress image set
+    }
+}
