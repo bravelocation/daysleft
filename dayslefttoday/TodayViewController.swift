eef3dc8fcd4cc9970ceca05c3387b576dd350bfb
John Pollard <johnp@bravelocation.com>
2016-08-02 13:55:41 +0100

    Set widget color in iOS10

--- dayslefttoday/TodayViewController.swift
+++ dayslefttoday/TodayViewController.swift
@@ -44,5 +44,21 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         self.counterView.counter = model.DaysGone(now)
         self.counterView.maximumValue = model.DaysLength
         self.counterView.updateControl()
+        
+        // Set widget colors based on version of iOS
+        let ios10AndAbove:Bool = NSProcessInfo.processInfo().isOperatingSystemAtLeastVersion(NSOperatingSystemVersion(majorVersion: 10, minorVersion: 0, patchVersion: 0))
+
+        if (ios10AndAbove) {
+            let darkGreen = UIColor(red: 53.0/255.0, green: 79.0/255.0, blue: 0.0, alpha: 1.0)
+            let lightTransparentColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 0.8)
+            
+            self.view.backgroundColor = lightTransparentColor
+            self.labelNumberTitle.textColor = darkGreen
+            self.labelPercentDone.textColor = darkGreen
+        } else {
+            self.view.backgroundColor = UIColor.clearColor()
+            self.labelNumberTitle.textColor = UIColor.whiteColor()
+            self.labelPercentDone.textColor = UIColor.whiteColor()
+        }
     }
 }
