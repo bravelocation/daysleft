66ea44a9e18fe438e83cc2c5d62c4fc9264e20de
John Pollard <johnp@bravelocation.com>
2016-09-14 11:28:18 +0100

    Adding background task to update complications

--- daysleft WatchKit Extension/ExtensionDelegate.swift
+++ daysleft WatchKit Extension/ExtensionDelegate.swift
@@ -14,7 +14,48 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         
     func applicationDidBecomeActive() {
         NSLog("applicationDidBecomeActive started")
-        self.model.initialiseWatchSession()        
+                
+        // Initialise the watch session
+        self.model.initialiseWatchSession()
+        
+        // Setup background refresh
+        self.setupBackgroundRefresh()
+        
         NSLog("applicationDidBecomeActive completed")
+    }
+    
+    func handleBackgroundTasks(backgroundTasks: Set<WKRefreshBackgroundTask>) {
+        print("Handling background task started")
+
+        // Mark tasks as completed
+        for task in backgroundTasks {
+            // If it was a background task, update complications and setup a new one
+            if (task is WKApplicationRefreshBackgroundTask) {
+                
+                // Simply update the complications on a background task being triggered
+                self.model.updateComplications()
+                
+                // Setup new refresh for tomorrow
+                self.setupBackgroundRefresh()
+            }
+            
+            task.setTaskCompleted()
+        }
+    }
+    
+    func setupBackgroundRefresh() {
+        // Setup a background refresh for 0100 tomorrow
+        let globalCalendar = NSCalendar.autoupdatingCurrentCalendar()
+        let startOfTodayComponents = globalCalendar.components([.Year, .Month, .Day], fromDate: NSDate())
+        let startOfToday = globalCalendar.dateFromComponents(startOfTodayComponents)
+        let tomorrowMorning = startOfToday!.addTimeInterval(25.0 * 60.0 * 60.0)
+        
+        WKExtension.sharedExtension().scheduleBackgroundRefreshWithPreferredDate(tomorrowMorning as! NSDate, userInfo: nil, scheduledCompletion: { (error: NSError?) in
+            if let error = error {
+                print("Error occurred while scheduling background refresh: \(error.localizedDescription)")
+            }
+        })
+        
+        print("Setup background task for \(tomorrowMorning)")
     }
 }
