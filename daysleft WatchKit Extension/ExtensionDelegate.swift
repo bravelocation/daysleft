cd0d664a791c9c6eef28c1c7631e17f63cac9f73
John Pollard <johnp@bravelocation.com>
2017-12-07 17:55:48 +0000

    Removed obsolete code

--- daysleft WatchKit Extension/ExtensionDelegate.swift
+++ daysleft WatchKit Extension/ExtensionDelegate.swift
@@ -39,7 +39,7 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
                 self.setupBackgroundRefresh()
             }
             
-            task.setTaskCompleted()
+            task.setTaskCompletedWithSnapshot(true)
         }
     }
     
