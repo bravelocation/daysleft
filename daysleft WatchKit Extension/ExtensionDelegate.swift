02868040487db9e6d64293f5615b8f92039b13aa
John Pollard <johnp@bravelocation.com>
2022-09-22 16:33:43 +0100

    Revert "Update complicationMake sure complications are updated when the watch app is made actives on app becoming active"
    This reverts commit 36aff3d44b09b0189569c1c7b8cbd3029cbc8246.

--- daysleft WatchKit Extension/ExtensionDelegate.swift
+++ daysleft WatchKit Extension/ExtensionDelegate.swift
@@ -41,9 +41,6 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         // Setup background refresh
         self.setupBackgroundRefresh()
         
-        // Update complications
-        self.updateComplications()
-        
         print("applicationDidBecomeActive completed")
     }
     
@@ -56,7 +53,14 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
             if (task is WKApplicationRefreshBackgroundTask) {
                 
                 // Simply update the complications on a background task being triggered
-                self.updateComplications()
+                let complicationServer = CLKComplicationServer.sharedInstance()
+                let activeComplications = complicationServer.activeComplications
+                
+                if (activeComplications != nil) {
+                    for complication in activeComplications! {
+                        complicationServer.reloadTimeline(for: complication)
+                    }
+                }
                 
                 // Also update the data model
                 self.dataModel.updateViewData()
@@ -67,6 +71,11 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
             
             task.setTaskCompletedWithSnapshot(true)
         }
+    }
+    
+    func handle(_ userActivity: NSUserActivity) {
+        print("Handling user activity")
+        // Not sure what to do here
     }
     
     // MARK: Event handlers
