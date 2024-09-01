37e9b9842b672c0fa24d7c6dbe84e2d4b78148b5
John Pollard <johnp@bravelocation.com>
2024-09-01 09:01:31 +0100

    Improved widget background

--- DaysLeftWidget/Views/WidgetBackgroundModifier.swift
+++ DaysLeftWidget/Views/WidgetBackgroundModifier.swift
@@ -9,10 +9,29 @@
 import SwiftUI
 
 struct WidgetBackgroundModifier: ViewModifier {
+    @Environment(\.colorScheme) var colorScheme
+    
+    let widgetDarkBackgroundGradient = LinearGradient(
+        colors: [
+            Color(red: 34.0/255.0, green: 34.0/255.0, blue: 37.0/255.0),
+            Color(red: 44.0/255.0, green: 44.0/255.0, blue: 47.0/255.0)
+        ],
+        startPoint: .top,
+        endPoint: .bottom)
+    
+    let widgetLightBackgroundGradient = LinearGradient(
+        colors: [
+            Color(red: 251.0/255.0, green: 251.0/255.0, blue: 255.0/255.0),
+            Color(red: 241.0/255.0, green: 241.0/255.0, blue: 248.0/255.0)
+        ],
+        startPoint: .top,
+        endPoint: .bottom)
+    
+    
     func body(content: Content) -> some View {
         if #available(iOS 17.0, macOS 14.0, watchOS 10.0, *) {
             content
-                .containerBackground(Color("WidgetBackground"), for: .widget)
+                .containerBackground(colorScheme == .dark ? self.widgetDarkBackgroundGradient : self.widgetLightBackgroundGradient, for: .widget)
         } else {
             content
         }
